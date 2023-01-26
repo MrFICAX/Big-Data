0081e9729978d62116ed1f46dcd3f3a16091559d
MrFICAX <ficax.al@elfak.rs>
2023-01-26 21:30:40 +0100

    Analytics finished

--- Docker/GeoLifeAnalysisApp.py
+++ Docker/GeoLifeAnalysisApp.py
@@ -5,22 +5,84 @@ from pyspark.sql.types import StructType
 # from pyspark.sql.functions import split, col
 from pyspark.sql.functions import *
 import pyspark.sql.functions as F
+import copy
+from pyspark.sql.types import IntegerType
 
-def filterDataFrameByLatitude(latMin, latMax):
-    return df.filter((df.lat >= latMin) & (df.lat <= latMax)).show()
 
-def filterDataFrameByLongitude(lonMin, lonMax):
-    return df.filter((df.lon >= lonMin) & (df.lon <= lonMax)).show()
+def filterDataFrameByLatitude(df, latMin, latMax):
+    print("filterDataFrameByLatitude entered")
+    return df.filter((df.lat >= latMin) & (df.lat <= latMax))
 
-def filterDataFrameByYear(yearMin, yearMax):
-    return df.filter((df.year >= yearMin) & (df.year <= yearMax)).show()
+def filterDataFrameByLongitude(df, lonMin, lonMax):
+    print("filterDataFrameByLongitude entered")
+    return df.filter((df.lon >= lonMin) & (df.lon <= lonMax))
+
+def filterDataFrameByAltitude(df, altMin, altMax):
+    return df.filter((df.alt >= altMin) & (df.alt <= altMax))    
+
+def filterDataFrameByYear(df, yearMin, yearMax):
+    print("filterDataFrameByYear entered")
+    return df.filter((df.year >= yearMin) & (df.year <= yearMax))
     
-def filterDataFrameByMonth(monthMin, monthMax):
-    return df.filter((df.year >= monthMin) & (df.month <= monthMax)).show()
+def filterDataFrameByMonth(df, monthMin, monthMax):
+    return df.filter((df.month >= monthMin) & (df.month <= monthMax))
 
-def filterDataFrameByDay(dayMin, dayMax):
-    return df.filter((df.day >= dayMin) & (df.day <= dayMax)).show()
+def filterDataFrameByDay(df, dayMin, dayMax):
+    return df.filter((df.day >= dayMin) & (df.day <= dayMax))
 
+def filterDataFrameByHour(df, hourMin, hourMax):
+    return df.filter((df.hour >= hourMin) & (df.hour <= hourMax))
+    
+def filterDataFrameByMinute(df, minuteMin, minuteMax):
+    return df.filter((df.minute >= minuteMin) & (df.minute <= minuteMax))
+
+def filterDataFrameBySecond(df, secondMin, secondMax):
+    return df.filter((df.second >= secondMin) & (df.second <= secondMax))
+
+def readInputValues():
+    delimiterFound = False
+    features = {}
+    for arg in sys.argv[2:]:
+        if arg == "|":
+            delimiterFound = True
+            continue
+        if not delimiterFound:
+            if arg not in features.keys():
+                features[arg] = []
+        else:
+            for key in features.keys():
+                if (len(features[key]) != 2):
+                    features[key].append(float(arg))
+                    break
+    return features
+
+def findDfBasedOnInputValues(inputDictionary, inputDf):
+        #tmpDf = inputDf.copy()
+        tmpDf = inputDf.alias('tmpDf')
+        tmpDf.show(20)
+        switcher = {
+                "lat": filterDataFrameByLatitude,
+                "lon": filterDataFrameByLongitude,
+                "alt": filterDataFrameByAltitude,
+                "year": filterDataFrameByYear,
+                "month": filterDataFrameByMonth,
+                "day": filterDataFrameByDay,
+                "hour": filterDataFrameByHour,
+                "minute": filterDataFrameByMinute,
+                "second": filterDataFrameBySecond,
+            }
+        for key in inputDictionary.keys():
+            method = switcher.get(key, "nothing")
+            if (method != "nothing"):
+                tmpDf = method(tmpDf, inputDictionary[key][0], inputDictionary[key][1])
+                printLine()
+                print("Filtered by key: " + key)
+                tmpDf.show(20)
+        return tmpDf
+def printLine():
+    print("*******************************************************************************")
+    print("-------------------------------------------------------------------------------")
+    print("*******************************************************************************")
 
 if __name__ == '__main__':
     # if len(sys.argv) != 2:
@@ -30,17 +92,17 @@ if __name__ == '__main__':
     appName = "GeoLifeGpsAnalysis"
 
     conf = SparkConf()
-    #conf.setMaster("spark://spark-master:7077")
-    conf.setMaster("local")
+    conf.setMaster("spark://spark-master:7077")
+    #conf.setMaster("local")
     conf.set("spark.driver.memory","4g")
 
     spark = SparkSession.builder.config(conf=conf).appName(appName).getOrCreate()
 
     geoLifeSchema = StructType() \
                     	.add("time", "string")\
-                    	.add("lat", "double")\
-                    	.add("lon", "double")\
-                        .add("alt", "double")\
+                    	.add("lat", "float")\
+                    	.add("lon", "float")\
+                        .add("alt", "float")\
                     	.add("label", "string")\
                     	.add("user", "integer")
                     	#.add("", "integer")\
@@ -50,13 +112,10 @@ if __name__ == '__main__':
     #booksdata=spark.read.csv("app/geolife_gps_reduced.csv", schema=geoLifeSchema)
 
     geoLifeDataFrame = spark.read.option("header", True).csv(sys.argv[1], schema=geoLifeSchema)
-    #geoLifeDataFrame=spark.read.option("header", True).csv("../geolife_gps_data_example.csv", schema=geoLifeSchema)
-    geoLifeDataFrame.show(5)
-    geoLifeDataFrame.printSchema()
-   
-    # df = booksdata.filter(booksdata.user == sys.argv[2])
-    # df.show()
-    
+                    #geoLifeDataFrame=spark.read.option("header", True).csv("../geolife_gps_data_example.csv", schema=geoLifeSchema)
+    # geoLifeDataFrame.show(5)
+    # geoLifeDataFrame.printSchema()
+       
     splitDFTime = geoLifeDataFrame.withColumn("date",split(col("time")," ").getItem(0))\
         .withColumn("exacttime",split(col("time")," ").getItem(1))\
         .drop("time")
@@ -67,6 +126,7 @@ if __name__ == '__main__':
         .withColumn("month",split(col("date"),"-").getItem(1))\
         .withColumn("day",split(col("date"),"-").getItem(2))\
         .drop("date")
+
     # splitDFYear.printSchema()
     # splitDFYear.show()
 
@@ -86,17 +146,49 @@ if __name__ == '__main__':
 
  
     df.show(20)
-    #df.select('user').distinct().show(2000)
+    df.printSchema()
 
-    option = sys.argv[2]
+    # df= df.withColumn("year",col("year").cast(IntegerType))\
+    #         .withColumn("month",col("month").cast(IntegerType))\
+    #          .withColumn("day",col("day").cast(IntegerType))\
+    #           .withColumn("hour",col("hour").cast(IntegerType))\
+    #            .withColumn("minute",col("minute").cast(IntegerType))\
+    #             .withColumn("second",col("second").cast(IntegerType))
 
-    print(sys.argv[1], sys.argv[2], sys.argv[3])
+    cols = ["year", "month", "day", "hour", "minute", "second"]
+    for col_name in cols:
+        df = df.withColumn(col_name, col(col_name).cast('float'))
+
+    df.show(20)
+    df.printSchema() 
+    
+
+    inputValuesDictionary = readInputValues()
+    print(inputValuesDictionary)
+
+    filteredDf = findDfBasedOnInputValues(inputValuesDictionary, df)
+    filteredDf.printSchema()
+    filteredDf.show(25)
+    filteredDf.describe().show()
+
+    #SHOWING ALL USERS AT EXACT PARAMETER VALUES
+    df.select('user').distinct().show(2000)
+
+    df.select('label').distinct().show(2000)
+
+
+                
+
 #OVO RADI
     #df.filter(df['lat'] >= sys.argv[2]).filter(df['lat'] <= sys.argv[3]).show()
 #OVO ISTO RADI
-    df.filter((df.lat >= sys.argv[2]) & (df.lat <= sys.argv[3])).show()
+    #df.filter((df.lat >= sys.argv[2]) & (df.lat <= sys.argv[3])).show()
 
-    df.show(20)
+
+
+
+
+    #df.show(20)
 
     #print(df.dropDuplicates(["user"]).select("user").collect())
     # filteredDF = df.where(df.lat >= 39).where(df.lat <= 40).select()
